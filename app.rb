1eb3271481a4a6096fab67a19cb141be2107060f
miso160 <hotmisosoup160@gmail.com>
2020-11-19 12:26:39 +0900

    変数修正

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ require 'pg'
 def connect_data(sql, values = nil)
   connect = PG.connect(host: 'localhost', user: 'postgres', password: '', dbname: 'memoapp', port: '5432')
   if sql.include?('SELECT')
-    results = connect.exec(sql, values)
-    @memos = results
+    @memos = connect.exec(sql, values)
   else
     connect.exec(sql, values)
   end
@@ -26,8 +25,8 @@ end
 
 get '/memos' do
   @title = 'memo top'
-  @sql = 'SELECT * FROM Memos'
-  connect_data(@sql)
+  sql = 'SELECT * FROM Memos'
+  connect_data(sql)
   erb :index
 end
 
@@ -38,44 +37,42 @@ end
 
 get '/memos/:id' do
   @title = 'show memo'
-  @id = params['id']
-  @sql = 'SELECT memo_title, memo_body FROM Memos WHERE memo_id = $1'
-  connect_data(@sql, [@id])
-  @title = @memos[0]['memo_title']
-  @body = @memos[0]['memo_body']
+  id = params['id']
+  sql = 'SELECT * FROM Memos WHERE memo_id = $1'
+  connect_data(sql, [id])
+  @memo = @memos[0]
   erb :showmemo
 end
 
 get '/memos/:id/edit' do
   @title = 'edit memo'
-  @id = params['id']
-  @sql = 'SELECT memo_title, memo_body FROM Memos WHERE memo_id = $1'
-  connect_data(@sql, [@id])
-  @title = @memos[0]['memo_title']
-  @body = @memos[0]['memo_body']
+  id = params['id']
+  sql = 'SELECT * FROM Memos WHERE memo_id = $1'
+  connect_data(sql, [id])
+  @memo = @memos[0]
   erb :editmemo
 end
 
 post '/memos' do
-  @title = params['title']
-  @body = params['body']
-  @sql = 'INSERT INTO Memos (memo_title, memo_body) VALUES ($1, $2)'
-  connect_data(@sql, [@title, @body]) unless @title.match(/^\s*$/)
+  title = params['title']
+  body = params['body']
+  sql = 'INSERT INTO Memos (memo_title, memo_body) VALUES ($1, $2)'
+  connect_data(sql, [title, body]) unless title.match(/^\s*$/)
   redirect_to_top
 end
 
 delete '/memos/:id' do
-  @id = params['id']
-  @sql = 'DELETE FROM Memos WHERE memo_id = $1'
-  connect_data(@sql, [@id])
+  id = params['id']
+  sql = 'DELETE FROM Memos WHERE memo_id = $1'
+  connect_data(sql, [id])
   redirect_to_top
 end
 
 patch '/memos/:id' do
-  @id = params['id']
-  @title = params['title']
-  @body = params['body']
-  @sql = 'UPDATE Memos SET memo_title = $1, memo_body = $2 WHERE memo_id = $3'
-  connect_data(@sql, [@title, @body, @id])
+  id = params['id']
+  title = params['title']
+  body = params['body']
+  sql = 'UPDATE Memos SET memo_title = $1, memo_body = $2 WHERE memo_id = $3'
+  connect_data(sql, [title, body, id])
   redirect_to_top
 end
